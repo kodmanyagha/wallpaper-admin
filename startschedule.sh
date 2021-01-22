f8da9cc3dd39fe8813f3b5647e2c9419268b1618
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2021-01-22 17:05:04 +0300

    Update startschedule.sh

--- startschedule.sh
+++ startschedule.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-nohup php artisan schedule:work --daemon > storage/logs/schedule.log &
+nohup php artisan schedule:work > storage/logs/schedule.log &
